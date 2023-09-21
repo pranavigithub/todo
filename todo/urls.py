0d5edf260c539d133dd49753c4bd04db3a6aa5c6
Pranavigithub <meenigapranavi27@gmail.com>
2023-09-21 23:07:46 +0530

    todo app final update

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,9 @@ from . import views
 urlpatterns=[
     path("", views.home, name ="home"),
     path("add_task/", views.add_task, name="add_task"),
+    path("mark_as_done/<int:pk>/", views.mark_as_done, name = "mark_as_done"),
+    path("mark_as_undone/<int:pk>/", views.mark_as_undone, name = "mark_as_undone"),
+    path("edit/<int:pk>/", views.edit, name="edit"),
+    path("delete/<int:id>/", views.delete, name="delete"),
+
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from django.http import HttpResponse
 from .models import Task
 def home(request):
     tasks = Task.objects.filter(is_completed = False).order_by ("-update_at")
+    completed_tasks= Task.objects.filter(is_completed=True)
+
     context ={
         'all_tasks': tasks,
+        'completed_tasks':completed_tasks,
     }
     return render(request, "home.html", context)
 
@@ -12,3 +15,35 @@ def add_task(request):
     task = request.POST['task']
     Task.objects.create(task = task)
     return redirect("home")
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect(home)
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect(home)
+
+def edit(request,pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context = {
+            'get_task': get_task,
+        }
+        return render(request, 'edit_task.html', context)
+
+def delete(request, id):
+    del_task = get_object_or_404(Task, pk=id)
+    del_task.delete()
+    return redirect('home')
+
+
